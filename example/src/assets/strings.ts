cf8bf7726765406c1c1ca1603cf1b1297288799b
Harrison Juma Ang'ong'a <jumaharrison1@gmail.com>
2023-01-31 11:43:57 +0300

    chore: republish branch

--- example/src/assets/strings.ts
+++ example/src/assets/strings.ts
@@ -1,6 +1,6 @@
 export const buttonLabels = {
   REGISTER_USER_WITH_BIOMETRICS: 'Register User With Biometrics',
   ADD_USER_DATA: 'Add User Data',
-  DENY_CONSENT: 'Deny Consent',
+  DENY_CONSENT: 'Deny onsent',
   GRANT_CONSENT: 'Grant Consent',
 };
